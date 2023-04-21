b3f0328080d6f6cae44e09d059cb41b75b34d86d
Vibhu Garg <vibhu.garg@gojek.com>
2023-04-21 23:24:03 +0530

    first commit

---
+++ src/test/java/com/vibhugarg/cms/couponmanagement/CouponManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibhugarg.cms.couponmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
